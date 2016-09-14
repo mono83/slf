2649f2734917a492e079beaaf0b6c6c9959c3477
Gotterdemarung <gotterdemarung@gmail.com>
2016-09-14 07:55:04 +0300

    mutator builder

--- mutator.go
+++ mutator.go
@@ -4,3 +4,16 @@ package slf
 type Mutator interface {
 	Modify(*Event)
 }
+
+// FuncMutator returns Mutator built over func
+func FuncMutator(f func(*Event)) Mutator {
+	return callableMutator{invoke: f}
+}
+
+type callableMutator struct {
+	invoke func(*Event)
+}
+
+func (cm callableMutator) Modify(e *Event) {
+	cm.invoke(e)
+}
